792fdd9ed920027f2b8cf8db06ea04e742023ecf
graceco22 <graceco921@gmail.com>
2024-07-28 19:38:42 -0700

    method 2

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
@@ -11,30 +7,32 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-const password = "ILoveProgramming"
-var enter = "";
 
-app.use(bodyParser.urlencoded({extended:true}));
+var userIsAuthorised = false;
 
-function checkPassword(req, res, next) {
-    enter = req.body["password"];
-    next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
 }
-
-app.use(checkPassword);
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-    if(enter === password ) {
-        res.sendFile(__dirname + "/public/secret.html");
-    } else {
-        res.sendFile(__dirname + "/public/index.html");
-    }
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
 });
 
-app.listen(port, ()=> {
-    console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
